0f3937c31ff7e8913c936d81c9c1a5f97acaf0d3
ifeelpankaj <ifeelpankaj@gmail.com>
2022-11-07 17:07:34 +0530

    New course pattern

--- models/Course.js
+++ models/Course.js
@@ -5,34 +5,28 @@ const schema = new mongoose.Schema({
     type: String,
     required: [true, "Please Enter  Title"],
   },
-  Course: [
-    {
-      subtitle: {
-        type: String,
-        required: [true, "Please Enter Sub Title"],
-      },
-      links: {
-        type: String,
-        required: true,
-      },
-      image: {
-        type: String,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-      category: {
-        type: String,
-        required: [true, "Please select the subject"],
-      },
 
-      author: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
+  subtitle: {
+    type: String,
+    required: [true, "Please Enter Sub Title"],
+  },
+  links: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  description: {
+    type: String,
+    required: [true, "Please select the subject"],
+  },
+
+  author: {
+    type: String,
+    required: true,
+  },
 });
 
 export const Course = mongoose.model("Course", schema);
