d533afbbffea9d3f975c3e45e19daf0659ffcbc8
ifeelpankaj <ifeelpankaj@gmail.com>
2022-12-11 20:01:22 +0530

    base64 error

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.use(
       },
   })
 );
+app.use(express.json({ limit: "50mb" })); 
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
